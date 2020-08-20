562f2d4ecbda8c7f0c35317a02ae19cf69f7b17e
jflopezr11 <joshualopezroque@gmail.com>
2020-08-19 19:03:39 -0700

    added new script

--- Quote_Generator/script.js
+++ Quote_Generator/script.js
@@ -1,11 +1,17 @@
 // Get Quote from API
 
 async function getQuote() {
+    const proxyUrl = 'https://arcane-temple-17000.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
-        const response = await 
+        const response = await fetch(proxyUrl + apiUrl);
+
+        const data = await response.json();
+
+        console.log(data)
     } catch (error) {
-        console.log('yikes, no quote, bro', error)
+        getQuote(); 
+        console.log('yikes, no quote, bro', error);
     }
 
 }
