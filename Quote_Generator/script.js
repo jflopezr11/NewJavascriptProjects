7a805cf60f9eb618afffcea74a31c92c47bc0e82
jflopezr11 <joshualopezroque@gmail.com>
2020-08-19 19:47:00 -0700

    project finished.

--- Quote_Generator/script.js
+++ Quote_Generator/script.js
@@ -3,9 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+//Show Loading
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+
+// Hide loader
+function removeLoadingSpinner() {
+    if(!loader.hidden) {
+        quoteContainer.hidden = false; 
+        loader.hidden = true; 
+    }
+}
 
 // Get Quote from API
 async function getQuote() {
+    showLoadingSpinner();
     const proxyUrl = 'https://arcane-temple-17000.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
@@ -23,12 +39,24 @@ async function getQuote() {
             quoteText.classList.remove('long-quote');
         }
         quoteText.innerText = data.quoteText; 
+        removeLoadingSpinner(); 
     } catch (error) {
         getQuote(); 
        
     }
+}
+
+function tweetQuote() {
+    const quote = quoteText.innerText; 
+    const author = authorText.innerText; 
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quote} - ${author}`;
+    window.open(twitterUrl, '_blank');
 
 }
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', getQuote);
+twitterBtn.addEventListener('click', tweetQuote);
 
 //on Load
 getQuote();
