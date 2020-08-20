899a9900d22b9db2b0c5d0e4f1b115dcde0ac76e
jflopezr11 <joshualopezroque@gmail.com>
2020-08-19 19:22:33 -0700

    api is working

--- Quote_Generator/script.js
+++ Quote_Generator/script.js
@@ -1,17 +1,31 @@
-// Get Quote from API
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
 
+// Get Quote from API
 async function getQuote() {
     const proxyUrl = 'https://arcane-temple-17000.herokuapp.com/'
     const apiUrl = 'http://api.forismatic.com/api/1.0/?method=getQuote&lang=en&format=json';
     try {
         const response = await fetch(proxyUrl + apiUrl);
-
         const data = await response.json();
-
-        console.log(data)
+        if (data.quoteAuthor === '') {
+            authorText.innerText = 'Unknown'
+        } else {
+            authorText.innerText = data.quoteAuthor;
+        }
+        //Reduce Font size for long quotes
+        if (data.quoteText.length > 120) {
+            quoteText.classList.add('long-quote');
+        } else {
+            quoteText.classList.remove('long-quote');
+        }
+        quoteText.innerText = data.quoteText; 
     } catch (error) {
         getQuote(); 
-        console.log('yikes, no quote, bro', error);
+       
     }
 
 }
