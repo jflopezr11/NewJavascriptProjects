c1c6a5fc3a740b0d8c96539c3f310ad4ae731b4b
jflopezr11 <joshualopezroque@gmail.com>
2020-08-25 18:24:31 -0700

    used localstorage

--- Light-Dark-Mode/script.js
+++ Light-Dark-Mode/script.js
@@ -33,9 +33,11 @@ function lightMode() {
 function switchTheme(event) {
     if (event.target.checked){
         document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme', 'dark');
         darkMode();
     }else {
-        document.documentElement.setAttribute('data-theme', 'light')
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme', 'light');
         lightMode(); 
 
     }
@@ -43,3 +45,11 @@ function switchTheme(event) {
 
 //EventListener
 toggleSwitch.addEventListener('change', switchTheme)
+
+const currentTheme = localStorage.getItem('theme');
+if(currentTheme) {
+    document.documentElement.setAttribute('data-theme', currentTheme);
+    if (currentTheme === 'dark');
+    toggleSwitch.checked = true; 
+    darkMode(); 
+} 
